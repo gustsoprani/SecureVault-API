30f2d3aa3a6d7f07db8dada4611630f53efe6179
Gustavo Rodrigues Soprani <soprani0gustavo@gmail.com>
2026-05-11 01:06:56 -0300

    feat: Configuracao do EF Core, Docker e entidade Secret

---
+++ SecureVault.Api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureVault.Api.Models;
+
+namespace SecureVault.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Secret> Secrets { get; set; }
+    }
+}
---
+++ SecureVault.Api/Models/Secret.cs
@@ -0,0 +1,9 @@
+﻿namespace SecureVault.Api.Models
+{
+    public class Secret
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EncryptedValue { get; set; }
+    }
+}
